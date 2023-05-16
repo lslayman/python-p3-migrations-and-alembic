73461fc2775ac7906d03d5d3edc697352bdf659e
Lauren Slayman <laurenaslayman@gmail.com>
2023-05-15 18:57:04 -0600

    completes code along

---
+++ .github/lib/migrations/versions/19ea832b0f18_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 19ea832b0f18
+Revises: 
+Create Date: 2023-05-15 18:01:06.470965
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '19ea832b0f18'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
